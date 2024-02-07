6649dcaa2bc7667009e42e22d137165c5f01bc62
Ferdinandy Chagas <ferdinandsc@yahoo.com.br>
2024-02-07 19:52:40 -0300

    comments included

--- menu/api/views.py
+++ menu/api/views.py
@@ -5,15 +5,19 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from menu.api.serializers import PizzaSerializer, Pizza
 
+# Classes que herdam de ModelViewSet já implementam o CRUD
 class PizzaViewSet(ModelViewSet):
+    # http://localhost:8000/api/pizzas/
     serializer_class = PizzaSerializer
     queryset = Pizza.objects.all()
     permission_classes = [ AllowAny ]
 
+    # Quando quiser adicionar uma nova funcionalidade use @action
+    # http://localhost:8000/api/pizzas/search/
     @action(methods=['get'], url_path='search', detail=False)
     def search(self, request):
-        pizzas = Pizza.objects.filter(border=False)
-        serializer = PizzaSerializer(pizzas, many=True)
+        pizzas = Pizza.objects.filter(border=False) #Consulta
+        serializer = PizzaSerializer(pizzas, many=True) #O resultado sendo convertido com serializer
         return Response(
-            {"info":"Pizzas encontradas", "data":serializer.data}, 
-            status=status.HTTP_200_OK)
+            {"info":"Pizzas encontradas", "data":serializer.data}, #Enviando o serializer como resposta
+            status=status.HTTP_200_OK) #Definido o código HTTP da resposta.
